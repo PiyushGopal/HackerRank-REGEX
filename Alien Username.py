660eea08d2933e79e7e7747f5b272004478ea9fa
Piyush Gopal <99497706+PiyushGopal@users.noreply.github.com>
2022-11-12 17:40:29 +0530

    Create Alien Username.py

---
+++ Alien Username.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
+pattern = re.compile('^[_\.][0-9]+[a-zA-Z]*[_]?$')
+n = int(input())
+for i in range(n):
+    x = str(input())
+    if(len(re.findall(pattern,x)) != 0):
+        print("VALID")
+    else:
+        print("INVALID")
