35954cf24d79e115b5bcb7f9598323cb31297e47
Piyush Gopal <99497706+PiyushGopal@users.noreply.github.com>
2022-11-12 18:55:37 +0530

    Create HackerRank Tweets.py

---
+++ HackerRank Tweets.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
+p = re.compile('((h|H)(a|A)(c|C)(k|K)(e|E)(r|R)(r|R)(a|A)(n|N)(k|K))')
+t = int(input())
+c = 0
+for i in range(t):
+    x = str(input())
+    c += len(re.findall(p, x))
+print(c)
